22d22d0930ce94cd77f355cd8e1493ae33a288e5
iossocket <avx302@gmail.com>
2026-03-16 20:28:48 +0800

    add starknet native token addresses

--- Sources/StarkNetKit/Types/Starknet.swift
+++ Sources/StarkNetKit/Types/Starknet.swift
@@ -77,3 +77,14 @@ public struct Starknet: Chain, Sendable, Equatable, Hashable {
     hasher.combine(chainId)
   }
 }
+
+// MARK: - Native Token Addresses
+
+extension Starknet {
+  public enum Token {
+    public static let ETH = Felt(
+      "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7")!
+    public static let STRK = Felt(
+      "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab072018f4287c938d")!
+  }
+}
